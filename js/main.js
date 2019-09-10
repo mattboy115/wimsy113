4502cb200164f6dbd1215d79573e9f62e548d56d
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2019-09-10 19:14:49 -0400

    changed background picture

--- js/main.js
+++ js/main.js
@@ -79,8 +79,8 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
-      typeSpeed: 100,
+      strings: ["Krystyna Ewing", "Designing.", "Coding.", "Freelancing.", "Illustrating"],
+      typeSpeed: 150,
       loop: true,
     });
   });
