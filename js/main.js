9d68113aa7362bf3ab3111cc3004498f9e9f6516
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2021-03-03 15:21:00 -0500

    updated links, header and footer color

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Krystyna Ewing", "Designing User Experiences", "Freelancing.", "Illustrating"],
+      strings: ["Designing User Experiences.", "Freelancing.", "Illustrating."],
       typeSpeed: 150,
       loop: true,
     });
