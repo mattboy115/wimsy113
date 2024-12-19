22765e8d7bb63258ca704f28c6ebbe2d0fd1e2cb
mattboy115 <matthew_c_lee@yahoo.com>
2024-12-19 16:54:24 -0500

    Created Home, Projects and edited css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import {Routes, Router, Route, Link } from react-router-dom;
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import Home from "./Home";
+import Projects from "./Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <ul></ul>
-        </nav>
-      </header>
-      <Router>
+  <Router>
+    <div className="app-body">
+      <nav className="nav">
+        <ul className="nav-list">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Projects">Projects</Link></li>
+          <li><Link to="./about/About">About</Link></li>
+        </ul>
+      </nav>
+      <div className="content">
         <Routes>
-          <Route>
-
-          </Route>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Projects" element={<Projects/>}/>
         </Routes>
-      </Router>
+      </div>
+      <footer>
+        Please show up
+      </footer>
     </div>
+  </Router>
   );
 }
 
