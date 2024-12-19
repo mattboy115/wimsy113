36d6f47c526eec416fed4393af4e316053119b92
mattboy115 <matthew_c_lee@yahoo.com>
2024-12-19 14:46:48 -0500

    Created new app

---
+++ portfolio/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import {Routes, Router, Route, Link } from react-router-dom;
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <ul></ul>
+        </nav>
+      </header>
+      <Router>
+        <Routes>
+          <Route>
+
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
