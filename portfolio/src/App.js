700309c0bd358bf5d1b6efc96761c6ddf741444a
mattboy115 <matthew_c_lee@yahoo.com>
2025-01-05 12:19:27 -0500

    fdsfs

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -31,7 +31,7 @@ function App() {
   }, []);
 
   return (
-  <Router>
+  <Router basename="wimsy113">
     <div className="app-body">
       <nav className={`nav ${isSticky ? "sticky" : ""}`}>
         <ul className="nav-list">
