2d2ce138882f8d88881e9aee2b8236f52491094e
mattboy115 <matthew_c_lee@yahoo.com>
2025-01-05 10:26:01 -0500

    fsdefs

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -36,7 +36,7 @@ function App() {
       <nav className={`nav ${isSticky ? "sticky" : ""}`}>
         <ul className="nav-list">
           <div className="home-button">
-            <Link to="/">
+            <Link to="/wimsy113">
               <img src={logo} alt="Logo for Home button"/>
             </Link>
           </div>
